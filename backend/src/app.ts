925acebb97dfcdc93db09f80202d7af84a1d60af
Eric Choo <ericchoo_0520@yahoo.com>
2023-12-29 22:34:30 +0800

    backend-chore: add eslint package

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,8 @@ import { ApolloServer } from "@apollo/server";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { expressMiddleware } from "@apollo/server/express4";
-import { graphql } from 'graphql'
 
-import { json } from 'body-parser';
+import { json } from "body-parser";
 import mongoose from "mongoose";
 import express from "express";
 import http from "http";
@@ -23,69 +22,74 @@ const httpServer = http.createServer(app);
 const port = ProcessENV.PORT || 8000;
 
 const schema = makeExecutableSchema({
-    typeDefs,
-    resolvers,
+	typeDefs,
+	resolvers,
 });
 
 const authSchema = makeExecutableSchema({
-    typeDefs: authTypeDefs,
-    resolvers: authResolvers,
+	typeDefs: authTypeDefs,
+	resolvers: authResolvers,
 });
 
-/** 
+/**
  * Main server - with auth middleware
  */
 const server = new ApolloServer({
-    schema,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-    introspection: ProcessENV.NODE_ENV !== 'production'
+	schema,
+	plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+	introspection: ProcessENV.NODE_ENV !== "production",
 });
 
-/** 
+/**
  * Auth server - without middleware
  */
 const authServer = new ApolloServer({
-    schema: authSchema,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-    introspection: ProcessENV.NODE_ENV !== 'production'
+	schema: authSchema,
+	plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+	introspection: ProcessENV.NODE_ENV !== "production",
 });
 
 /**
  * Connect database and start servers
  */
 async function startServers(): Promise<void> {
-    await mongoose.connect(ProcessENV.MONGODB, { autoIndex: false });
-    
-    console.log(chalk.blue("MongoDB Connection successful") + "🚀");
+	await mongoose.connect(ProcessENV.MONGODB, { autoIndex: false });
 
-    await server.start();
-    await authServer.start();
+	console.log(chalk.blue("MongoDB Connection successful") + "🚀");
 
-    app.use(
-        '/graphql',
-        cors<cors.CorsRequest>(),
-        json(),
-        expressMiddleware(server, {
-            context
-        }),
-    );
+	await server.start();
+	await authServer.start();
 
-    app.use(
-        '/auth',
-        cors<cors.CorsRequest>(),
-        json(),
-        expressMiddleware(authServer),
-    );
-    
-    await new Promise<void>((resolve) => httpServer.listen({ port }, resolve));
-    
-    console.log(chalk.blue(`Server running at port: `) + chalk.green(`http://localhost:${port}/graphql`));
-    console.log(chalk.blue(`Auth_Server running at port: `) + chalk.green(`http://localhost:${port}/auth`));
+	app.use(
+		"/graphql",
+		cors<cors.CorsRequest>(),
+		json(),
+		expressMiddleware(server, {
+			context,
+		})
+	);
+
+	app.use(
+		"/auth",
+		cors<cors.CorsRequest>(),
+		json(),
+		expressMiddleware(authServer)
+	);
+
+	await new Promise<void>((resolve) => httpServer.listen({ port }, resolve));
+
+	console.log(
+		chalk.blue(`Server running at port: `) +
+		chalk.green(`http://localhost:${port}/graphql`)
+	);
+	console.log(
+		chalk.blue(`Auth_Server running at port: `) +
+		chalk.green(`http://localhost:${port}/auth`)
+	);
 }
 
-if(!validTypeDef) {
-    console.log("---- Unvalid TypeDef -----")
+if (!validTypeDef) {
+	console.log("---- Unvalid TypeDef -----");
 } else {
-    startServers();
+	startServers();
 }
-
--- backend/src/context/context.ts
+++ backend/src/context/context.ts
@@ -7,52 +7,58 @@ import User from "../models/User";
 const verifyUser = async (token: string) => {
     try {
         if (token) {
-            const { user_id } = jwt.verify(token, ProcessENV.AUTH_TENANT_NAME) as {
-                user_id: string;
-            };
+            const jwtObj = jwt.verify(
+                token,
+                ProcessENV.AUTH_TENANT_NAME
+            ) as jwt.JwtPayload
 
-            return await User.findOne({ _id: user_id });   
-        };
+            return jwtObj
+        }
         return null;
     } catch (error) {
-        throw new Error('JWT verification failed');
-    };
+        throw new Error("JWT verification failed");
+    }
 };
 
-const context = async (context: ExpressContextFunctionArgument ) => {
+const context = async (context: ExpressContextFunctionArgument) => {
     const { req, res } = context;
-    if (req.body.operationName === 'IntrospectionQuery') {
+    if (
+        req.body.operationName === "IntrospectionQuery"
+        || req.headers["x-apollo-admin-secret"] === ProcessENV.GRAPHQL_SECRET
+    ) {
         return {};
-    };
+    }
 
     // get the user token from the headers
     const authHeader = req.headers.authorization;
 
     if (!authHeader) {
-        throw new GraphQLError('Authorization header must be provided', {
+        throw new GraphQLError("Authorization header must be provided", {
             extensions: {
-                code: 'AUTHORIZATION_HEADER_NOT_EXIST',
-                argumentName: 'authorization',
+                code: "AUTHORIZATION_HEADER_NOT_EXIST",
+                argumentName: "authorization",
                 http: { status: 401 },
             },
         });
-    };
+    }
 
-    const token = authHeader.split(' ')[1];
+    const token = authHeader.split(" ")[1];
 
     // Try to retrieve a user with the token
-    const currentUser = await verifyUser(token);
-    
+    const { user_id, iat, exp } = await verifyUser(token) as jwt.JwtPayload;
+
+    const currentUser = await User.findOne({ _id: user_id });
+
     if (!currentUser) {
-        throw new GraphQLError('User is not Authenticated', {
+        throw new GraphQLError("User is not Authenticated", {
             extensions: {
-                code: 'NOT_AUTHENTICATED',
-                argumentName: 'authentication',
+                code: "NOT_AUTHENTICATED",
+                argumentName: "authentication",
                 http: { status: 401 },
             },
         });
-    };
-   
+    }
+
     // add the user to the context
     return { currentUser };
 };
