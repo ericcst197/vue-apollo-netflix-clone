2747a8be411232e4e8118130ce44db2e069730a3
Eric Choo <ericchoo_0520@yahoo.com>
2024-01-12 12:58:55 +0800

    refactor: baseInput component

--- frontend/src/components/BaseInput.vue
+++ frontend/src/components/BaseInput.vue
@@ -20,7 +20,7 @@ interface Props {
     borderStyle?: string
 }
 
-const { id, type = 'text', label = "", placeholder , helperText = "", required, disabled = false, warn = false, modelValue, maxLength = null, theme = 'dark', hasClicked, borderStyle } = defineProps<Props>()
+const { id, type = "text", label = "", placeholder, helperText = "", required, disabled = false, warn = false, modelValue, maxLength, theme = "dark", hasClicked, borderStyle } = defineProps<Props>()
 const emit = defineEmits<{
     (e: 'update:modelValue', value: any): void
 }>()
@@ -29,9 +29,9 @@ const isFocused = ref(false)
 const isLabelEmpty = computed(() => label == "")
 const isPasswordShowed = ref(false)
 const themeClassMap = {
-    light: '!text-gray-300 !bg-white',
-    dark: '!text-gray-700 !bg-[#333]', // default
-    'dark-2': '!text-white !bg-zinc-900',
+    light: "!text-gray-700 !bg-white",
+    dark: "!text-gray-300 !bg-[#333]", // default
+    'dark-2': "!text-white !bg-zinc-900",
 }
 
 function setFocus(value: boolean) {
@@ -79,28 +79,29 @@ const handleInput = ($event) => {
             </div>
 
             <div class="relative flex-auto" :class="[borderStyle]">
-                <input :id="id" :value="modelValue" @input="handleInput" :type="inputType" @focusin="onFocus" @focusout="onUnFocus"
-                    :disabled="disabled" :placeholder="placeholder || ' '" :required="required" :maxlength="maxLength"
-                    class="block px-2.5 pb-2.5 pt-5 w-full text-white dark:text-gray-400 text-sm bg-transparent appearance-none focus:outline-none focus:ring-0 peer"
-                    :class="{'border-0 border-b-2 border-orange-500': warn, 'focus:bg-[#454545]': theme.includes('dark')}"/>
+                <input :id="id" :value="modelValue" @input="handleInput" :type="inputType" @focusin="onFocus"
+                    @focusout="onUnFocus" :disabled="disabled" :placeholder="placeholder || ' '" :required="required"
+                    :maxlength="maxLength"
+                    class="block px-2.5 pb-2.5 pt-5 w-full text-sm bg-transparent appearance-none focus:outline-none focus:ring-0 peer"
+                    :class="{ 'border-0 border-b-2 border-orange-500': warn, 'focus:bg-[#454545]': theme.includes('dark') }" />
                 <label :for="id"
-                    class="absolute text-sm text-white dark:text-gray-400 duration-300 transform -translate-y-4 scale-90 top-4 z-10 origin-[0] left-2.5
+                    class="absolute text-sm duration-300 transform -translate-y-4 scale-90 top-4 z-10 origin-[0] left-2.5
                     peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-90 peer-focus:-translate-y-4">
                     {{ label }}</label>
             </div>
 
             <button v-if="warn" type="button" class="absolute right-2 mx-3" disabled><img :src="WarningIcon" /></button>
-            <button v-if="type == 'password'" class="absolute right-2 mx-3" :class="{'!right-10': warn}"
-             type="button" @click="togglePassword">
-                <SvgIcon v-if="isPasswordShowed" :src="EyeIcon" class="stroke-2 stroke-gray-400" :height="24" :width="24"/>
-                <SvgIcon v-else :src="EyeOffIcon" class="stroke-2 stroke-gray-400" :height="24" :width="24"/>
+            <button v-if="type == 'password'" class="absolute right-2 mx-3" :class="{ '!right-10': warn }" type="button"
+                @click="togglePassword">
+                <SvgIcon v-if="isPasswordShowed" :src="EyeIcon" class="stroke-2 stroke-gray-400" :height="24" :width="24" />
+                <SvgIcon v-else :src="EyeOffIcon" class="stroke-2 stroke-gray-400" :height="24" :width="24" />
             </button>
 
             <div :class="{ 'ml-2': $slots.right }">
                 <slot name="right"></slot>
             </div>
         </div>
-        <p v-if="helperText && hasClicked" :class="{ 'text-orange-500': warn, 'text-gray-500': !warn }" class="text-xs mt-1.5">
+        <p v-if="helperText && warn" :class="{ 'text-orange-500': warn, 'text-gray-500': !warn }" class="text-xs mt-1.5">
             {{ helperText }}</p>
     </div>
 </template>
