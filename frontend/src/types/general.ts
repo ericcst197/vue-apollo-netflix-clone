e57347cb8ffef16bade91da9db4d6583dcd72762
Eric Choo <ericchoo_0520@yahoo.com>
2024-04-03 23:57:13 +0800

    chore: install youtube-player package

--- frontend/src/types/general.ts
+++ frontend/src/types/general.ts
@@ -1,1 +1,8 @@
 export type Writeable<T> = { -readonly [P in keyof T]: T[P] };
+import YouTubePlayer from 'youtube-player';
+
+declare global {
+    interface Window {
+        YouTubePlayer: typeof YouTubePlayer;
+    }
+}
